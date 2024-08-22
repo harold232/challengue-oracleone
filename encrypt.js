a53fee529c512829820ff02506deb7599d6bcfb5
Harold Aquino <harold.aquino@unmsm.edu.pe>
2024-08-21 21:39:58 -0500

    Decrypt button

--- encrypt.js
+++ encrypt.js
@@ -19,5 +19,39 @@ function decrypText(text) {
 }
 
 document.getElementById('encrypt-button').addEventListener('click', function() {
-    
+    const inputText = document.getElementById('text-input').value.toLowerCase();
+    const encryptedText = encrypText(inputText);
+
+    const outputArea = document.getElementById('output-area');
+
+    if (document.getElementById('encrypted-text')) {
+        document.getElementById('encrypted-text').textContent = encryptedText;
+    } else {
+        const outputText = document.createElement('p');
+        outputText.id = 'encrypted-text';
+        outputText.textContent = encryptedText;
+        outputArea.appendChild(outputText);
+    }
+
+    document.getElementById('output-image').style.display = 'none';
+    outputArea.style.display = 'block';
+});
+
+document.getElementById('decrypt-button').addEventListener('click', function() {
+    const inputText = document.getElementById('text-input').value.toLowerCase();
+    const decryptedText = decrypText(inputText);
+
+    const outputArea = document.getElementById('output-area');
+
+    if (document.getElementById('encrypted-text')) {
+        document.getElementById('encrypted-text').textContent = decryptedText;
+    } else {
+        const outputText = createElement('p');
+        outputText.id = 'encrypted-text';
+        outputText.textContent = decryptedText;
+        outputArea.appendChild(outputText);
+    }
+    console.log('area');
+    document.getElementById('output-image').style.display = 'none';
+    outputArea.style.display = 'block';
 });
