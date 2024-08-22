d2c4e686757dddf40957bbc3fe1563678194e9de
Harold Aquino <harold.aquino@unmsm.edu.pe>
2024-08-21 20:43:56 -0500

    Avance challengue

---
+++ encrypt.js
@@ -0,0 +1,23 @@
+function encrypText(text) {
+    let encryptedText = text
+    .replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/a/g, "ai")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat");
+    return encryptedText;
+}
+
+function decrypText(text) {
+    let decryptedText = text
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
+    return decryptedText;
+}
+
+document.getElementById('encrypt-button').addEventListener('click', function() {
+    
+});
